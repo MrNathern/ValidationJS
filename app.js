e81c699cf298d357263f162ae199a01d75f02225
MrNathern <89406118+MrNathern@users.noreply.github.com>
2021-10-31 19:29:23 -0400

    Initial Changes

---
+++ app.js
@@ -0,0 +1,29 @@
+var message=document.body.querySelector(".loop");
+
+var condition =true;
+
+var DragDamage=Math.floor(Math.random()*10)+1)
+
+message.innerHTML="Dragon dealt"
+
+
+
+
+
+
+
+// var PlayerHits = prompt("How many times do you want to hit the dragon? Only up to 5 times.");
+// var PlayerDamage=getRandomInt(PlayerHits)+1
+// if (PlayerDamage>5){
+//     TotalPlayerDamage++
+//     var DragonDamage=getRandomInt(2)+1
+//     PlayerHealth-DragonDamage
+//     message.innerHTML=("You've dealt one damage to the dragon! The dragon dealt "+DragonDamage+" damage to you!");
+// }else {
+//     var DragonDamage=getRandomInt(2)+1
+//     PlayerHealth-DragonDamage
+//     message.innerHTML=("You've dealt no damage to the dragon! The dragon dealt "+DragonDamage+" damage to you!");
+// }
+//
+// document.write("You have "+PlayerHealth+" health remaining.")
+
